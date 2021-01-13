3ed71550280acc6cdb38c293a9a16d294bd9b842
HtinSharKyaw <htinsharrkyaw@gmail.com>
2021-01-13 22:17:29 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react';
+import {CardList} from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();//it calls the constructor methods in the component class
+    this.state = {
+      monsters:[],
+      searchField:''
+    };
+  }
+    
+  componentDidMount(){
+    //console.log('component did mount is called');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monsters:users}));
+  }
+  
+  componentDidUpdate(){
+    //console.log('component did update is called');
+    console.log(this.state.searchField);
+  }
+
+  handleChange= e =>{
+    this.setState({searchField:e.target.value})
+  }
+
+  render(){
+
+    const {monsters,searchField}=this.state;
+    const filterMonsters = monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
+    return(
+      <div className="App">
+      <SearchBox 
+      placeholder='Search Monsters' 
+      handleChange={this.handleChange}/>
+      <CardList monsters={filterMonsters}/>
+      </div>
+    )
+  }
 }
 
 export default App;
